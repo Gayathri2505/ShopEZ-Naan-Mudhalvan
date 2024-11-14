56bb7897c7306dc2c5ed0f6d4a1574c595bae64d
Gayathri2505 <116957331+Gayathri2505@users.noreply.github.com>
2024-11-14 18:02:12 +0530

    Code Uploaded

--- ShopEZ/client/src/pages/customer/Profile.js
+++ ShopEZ/client/src/pages/customer/Profile.js
@@ -85,7 +85,7 @@ const Profile = () => {
 
     if (loading) return <p>Loading...</p>;
 
-    // Filter orders based on their status
+    // Filter orders based on their order status
     const activeOrders = orders.filter(order => order.orderStatus === "order placed" || order.orderStatus === "In-transit");
     const completedOrders = orders.filter(order => order.orderStatus === "delivered");
     const cancelledOrders = orders.filter(order => order.orderStatus === "cancelled");
